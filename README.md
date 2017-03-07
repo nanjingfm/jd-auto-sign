5b756fdbcf7e5496c95f70ef47cf8c971cd16c73
fuming <765390765@qq.com>
2017-03-08 01:04:56 +0800

    京东自动签到脚本

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# jd-auto-sign
+# jd自动签到脚本
+
+##安装(ubuntu)
+```shell
+sudo apt-get install python-pip
+pip install phantomjs
+pip install casperjs
+```
+
+##usage
+```shell
+/usr/local/bin/casperjs --username=你的jd账号 --password=你的jd密码 /root/login.js
+```
+
+## auto run
+在crontab中加入以下记录，运行策略自定义
+```shell
+0 5 * * * /usr/local/bin/casperjs --username=你的jd账号 --password=你的jd密码 /root/login.js >> /tmp/jdcheckin.log
+```
---
+++ login.js
@@ -0,0 +1,146 @@
+/**
+ * Created by FM on 2017/3/7.
+ */
+var utils = require('utils');
+var fs = require('fs');
+
+var standalone = false;
+if(typeof(casper) == 'undefined') {
+    standalone = true;
+    var casper = require('casper').create();
+}
+
+// removing default options passed by the Python executable
+casper.cli.drop("cli");
+casper.cli.drop("casper-path");
+
+if ((typeof(options) == 'undefined') && Object.keys(casper.cli.options).length === 0) {
+    casper.echo("Usage: --username=<username> --password=<password> --captcha=<captcha code> --cookie=<cookie in string format>");
+    if(standalone) casper.exit(1);
+}
+
+// don't use var options here as it will undefine pass-in options variable
+options  = (typeof(options) == 'undefined') ? casper.cli.options : options;
+
+var username = options["username"];
+var password = options["password"];
+var phonecode = options["phonecode"];
+var action = options["action"];
+var cookie = options["cookie"];
+var captcha = options["captcha"];
+
+var storepath = '/tmp/jdcheckin/cookies/';
+var cookiefile = storepath + username + '.cookie';
+printDate();
+if (!cookie) {
+    var storecookie = getCookies();
+    if (storecookie) {
+        cookie = storecookie;
+    }
+}
+
+if (cookie) {
+    phantom.cookies = JSON.parse(cookie);
+}
+
+casper.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36';
+casper.start('https://order.jd.com/center/list.action').then(function() {
+    if (checkLogin()) {
+        saveCookies(phantom.cookies);
+        checkIn();
+    } else {
+        login();
+    }
+});
+
+
+function login() {
+    casper.thenOpen('https://passport.jd.com/new/login.aspx', function() {
+        casper.waitForSelector('.login-tab-r', function() {
+            this.click('.login-tab-r');
+        });
+    });
+
+    casper.then(function() {
+        casper.capture('/tmp/1.png');
+        if (casper.exists('.verify-code') && casper.visible('.verify-code')) {
+            var captcha_url = this.getElementAttribute('.verify-code', 'src');
+            casper.echo(captcha_url);
+            casper.thenOpen(captcha_url, function(){
+                this.capture('/tmp/captcha.png');
+                casper.echo(JSON.stringify({
+                    'status': 'find-captcha',
+                    'cookie': phantom.cookies
+                })).exit(1);
+            })
+        } else {
+            this.fill('form#formlogin', {
+                'loginname': username,
+                'nloginpwd': password
+            }, false);
+        }
+    });
+
+    casper.then(function() {
+        this.click('#loginsubmit');
+        this.wait(3000, function() {
+            if (checkLogin()) {
+                saveCookies(phantom.cookies);
+                checkIn();
+            }
+        });
+    });
+}
+
+function checkLogin() {
+    var currenturl = casper.getCurrentUrl();
+    if (/passport\.jd\.com/g.exec(currenturl)) {
+        console.log('login error');
+        return false;
+    } else {
+        console.log('login success');
+        return true;
+    }
+}
+
+// 签到
+function checkIn() {
+    casper.thenOpen('https://vip.jd.com', function(){
+        casper.waitForSelector('.checkin-ready', function() {
+            this.click('.checkin-ready');
+            casper.capture('/tmp/2.png');
+            casper.echo('checkin success');
+        }, function timeout() {
+            if (casper.exists('.checkined')) {
+                casper.echo('今日已经签到');
+            } else {
+                casper.echo('系统错误');
+            }
+        }, 3000);
+    });
+}
+
+function saveCookies(cookies) {
+    if (!fs.isFile) {
+        fs.touch(cookiefile);
+    }
+    fs.write(cookiefile, JSON.stringify(cookies), 'w');
+}
+
+function getCookies() {
+    if (!fs.isDirectory(storepath)) {
+        fs.makeTree(storepath);
+    }
+    if (fs.isFile(cookiefile)) {
+        return fs.read(cookiefile);
+    } else {
+        return false;
+    }
+}
+
+function printDate() {
+    var date = new Date();
+    casper.echo('当前时间：' + date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate());
+}
+
+casper.run();
